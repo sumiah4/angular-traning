f0eb051d2a4da60b8c07882b3db46c9a4331fccc
Sumiah <sumiah4@gmail.com>
2025-01-17 17:52:41 +0530

    using ngSwitch select te role of user from a drop down list and display the div panel according to the role dynamic

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -20,7 +20,7 @@ export class GreetingComponent {
     { name: 'Vegetables', items: ['Carrot', 'Broccoli', 'Spinach'] }
   ];
 
-  //roleList:string[] = ['admin', 'manager', 'lead', 'employee'];
+  roleLists:string[] = ['','admin', 'manager', 'lead', 'employee'];
   role:string = '';
 
 }
