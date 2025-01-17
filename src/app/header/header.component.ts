e39e332e2134d831f76d703f991c9c5c29ab5984
Sumiah <sumiah4@gmail.com>
2025-01-17 14:51:07 +0530

    Using ngFor iterate the following user array and display details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,9 +12,16 @@ export class HeaderComponent {
   imageUrl:string = '/images/logo-in.png';
 
   paraContent:string = 'Hello theis is the new contest.I havent updated.';
+  users = [
+    { id: 1, name: 'John' },
+    { id: 2, name: 'Maryam' },
+    { id: 3, name: 'George' }
+  ];
 
   updateParaText(event:any) {   
     this.paraContent = event.target.value; 
   }
+
+  
 
 }
